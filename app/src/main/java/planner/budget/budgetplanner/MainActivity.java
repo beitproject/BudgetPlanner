df9348957b1776689df94b684bb2349cbd2f82d8
Gaurav <gauravambavle@gmail.com>
2018-01-20 13:07:37 +0530

    home page with barr graph and cards

--- app/src/main/java/planner/budget/budgetplanner/MainActivity.java
+++ app/src/main/java/planner/budget/budgetplanner/MainActivity.java
@@ -1,10 +1,16 @@
 package planner.budget.budgetplanner;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
+
+import layout.NavigationDrawerFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,10 +24,25 @@ public class MainActivity extends AppCompatActivity {
         toolbar= (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
 
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(false);
         NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment)
                 getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
         drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout)findViewById(R.id.drawer_layout), toolbar);
+
+
+            Button home_button = (Button)findViewById(R.id.home_button);
+            home_button.setOnClickListener(new Button.OnClickListener(){
+                public void onClick(View v) {
+                    try {
+                        Intent intent = new Intent(getApplicationContext(), homepage_1.class);
+                        startActivity(intent);
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+
     }
 
     @Override
---
+++ app/src/main/java/planner/budget/budgetplanner/homepage_1.java
@@ -0,0 +1,36 @@
+package planner.budget.budgetplanner;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.BarGraphSeries;
+import com.jjoe64.graphview.series.DataPoint;
+
+import java.util.ArrayList;
+
+public class homepage_1 extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage_1);
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        BarGraphSeries<DataPoint> series = new BarGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, -1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
+
+
+    }
+}
